ea5cfa9fdb87604d78ba14fbe7fc0cdd1153c385
deewondev <deewondev@gmail.com>
2023-06-15 15:51:59 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program displays a single line of text using printf function.
+ * Return: (0) if success, else code error.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
