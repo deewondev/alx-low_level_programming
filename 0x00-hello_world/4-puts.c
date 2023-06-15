a260836b9b7209ab1c4866ea122a7e8aad0fff19
deewondev <deewondev@gmail.com>
2023-06-15 15:13:21 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program display a line of text using puts function.
+ * Return: (0) if success, else code error.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
