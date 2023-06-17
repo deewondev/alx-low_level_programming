906d79fd95dd20e78eaff07b84ae269ed05334a3
deewondev <deewondev@gmail.com>
2023-06-16 19:36:08 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints three unidentical set of digits.
+ * Return: (0) if success, else code error.
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				if ((x != y && y != z && x < z) && (x < y && y < z && x < z))
+				{
+					putchar(x + 48);
+					putchar(y + 48);
+					putchar(z + 48);
+					if ((x + y + z) != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
