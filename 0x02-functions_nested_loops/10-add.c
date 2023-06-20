22f9a5eaa439582740a59e85baf3416dd1e0b7f5
deewondev <deewondev@gmail.com>
2023-06-20 15:37:17 -0700

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * description - This program adds two integers
+ *
+ * @num1: Parameter num1 accept first input from user
+ *
+ * @num2: Parameter num2 accept second input from user
+ *
+ * Return: 0 if success, else code error
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
