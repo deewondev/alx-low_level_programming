65a73586c87ad052c955d7964e9f37ae64362ca0
deewondev <deewondev@gmail.com>
2023-06-24 05:44:18 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,23 +18,23 @@ int main(void)
 	{
 		if (x % 15 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (x % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (x % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
+		}
+		else if (x != 1 && x < 100)
+		{
+			printf(" %d", x);
 		}
 		else
 		{
-			printf("%i", x);
-		}
-		if (x < 100)
-		{
-			printf(" ");
+			printf("%d", x);
 		}
 	}
 	printf("\n");
