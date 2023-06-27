82c758d919d88947ba436d203bac0739bc900576
deewondev <deewondev@gmail.com>
2023-06-26 19:25:44 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy string pointed by src including terminating null byte \0
+ *
+ * @dest: Pointer return value as copy to
+ *
+ * @src: Pointer parameter as copy from
+ *
+ * Return: dest pointer as string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0, count;
+
+	while (*(src + a) != '\0')
+	{
+		a++;
+	}
+	for (count = 0; count < a; count++)
+	{
+		if (count != a)
+		{
+			dest[count] = src[count];
+		}
+		else
+		{
+			dest[count] = '\0';
+		}
+	}
+	return (dest);
+}
