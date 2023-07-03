4b693c7f45f4990f8d2c11fe4cfc0dd15d4e9717
deewondev <deewondev@gmail.com>
2023-07-03 03:12:18 -0700

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - Function that copies memory area
+ *
+ * @dest: First parameter string
+ *
+ * @src: Second parameter string
+ *
+ * @n: Third parameter integer
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+
+	return (dest);
+}
