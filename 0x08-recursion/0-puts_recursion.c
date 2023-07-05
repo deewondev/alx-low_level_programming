a1ab9e2e62561c6ccd341814ffab653341187f74
deewondev <deewondev@gmail.com>
2023-07-05 06:24:15 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Function that prints a string, followed by a line
+ *
+ * @s: Parameter pointer
+ *
+ * Return: void or nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
