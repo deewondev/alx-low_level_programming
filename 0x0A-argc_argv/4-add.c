8ffc24caa4fa4a27bd1888758307cf916bed0bf8
deewondev <deewondev@gmail.com>
2023-07-07 15:03:43 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,12 @@ int main(int argc, char *argv[])
 {
 	int x, y, sum;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	for (x = 1; x < argc; x++)
 	{
 		for (y = 0; argv[x][y] != '\0'; y++)
