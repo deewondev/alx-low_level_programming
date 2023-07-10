7fdf9d038e6562f9fd4ff72c8603a9481649b896
deewondev <deewondev@gmail.com>
2023-07-10 15:08:15 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,16 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *ptr = malloc(size * sizeof(char));
+	char *ptr;
 
-	if (size == 0 || ptr == NULL)
+	if (size == 0)
 		return (NULL);
+
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
