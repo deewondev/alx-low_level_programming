be3e5f000e2368da13664b7526f8bec19f1421b1
deewondev <deewondev@gmail.com>
2023-07-10 12:22:18 -0700

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * alloc_grid - program that returns to a 2D array of integers
+ * @width: rows of array integer
+ * @height: columns of array integer
+ * Return: integer else NULL if width or height is 0 or negative
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, **array2D;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	array2D = malloc(sizeof(int *) * width);
+
+	for (i = 0; i < width; i++)
+	{
+		array2D[i] = malloc(sizeof(int) * height);
+	}
+
+	return (array2D);
+}
