0c99613aecd561c1b60f4cccd673a1443acef4b2
deewondev <deewondev@gmail.com>
2023-07-13 14:25:20 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,8 +17,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i, size;
 	char *newptr;
 
-	ptr = malloc(old_size);
-
 	if (new_size == old_size)
 		return (ptr);
 
