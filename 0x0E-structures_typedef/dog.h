552e357ff51f363e9ae61dc06035874ee0291535
deewondev <deewondev@gmail.com>
2023-07-17 08:04:46 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_MACRO_H
+#define DOG_MACRO_H
+
+/**
+ * struct dog - define a new type struct dog
+ * @name: name of struct dog
+ * @age: age of struct dog
+ * @owner: owner's name of struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
