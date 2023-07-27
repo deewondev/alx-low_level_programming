031343a0df61fb6e59d52d39c806737cc3b1fe7f
deewondev <deewondev@gmail.com>
2023-07-27 08:57:46 -0700

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer to list_t list to be freed
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
