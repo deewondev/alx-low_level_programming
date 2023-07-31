ff22a1149c92abf8fa3129eabae48d41b4765b0e
deewondev <deewondev@gmail.com>
2023-07-31 06:01:36 -0700

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements
+ * in a linked listint_t list
+ * @h: pointer to next node in listint_t
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	int num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
