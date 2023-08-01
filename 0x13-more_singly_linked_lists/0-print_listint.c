cd142aa36d7cf8e023cfdc43893c0f4f20de0f59
deewondev <deewondev@gmail.com>
2023-07-31 18:24:57 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - function that prints all the elements of a listint_t list
- * @h: pointer to next node of listint_t
+ * @h: pointer to next node in listint_t
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
