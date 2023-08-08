c483c7491673b4fa8496c58c9c43114eba12d2b6
deewondev <deewondev@gmail.com>
2023-08-07 23:04:06 -0700

    main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,15 +14,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-void _elfchecker(unsigned char *str);
-void _print_magic(unsigned char *str);
-void _print_class(unsigned char *str);
-void _print_data(unsigned char *str);
-void _print_version(unsigned char *str);
-void _print_abi(unsigned char *str);
-void _print_osabi(unsigned char *str);
-void _print_type(unsigned int type, unsigned char *str);
-void _print_entry(unsigned long int entry, unsigned char *str);
-void _elfclose(int elf);
-
 #endif /* MAIN_HEADER */
